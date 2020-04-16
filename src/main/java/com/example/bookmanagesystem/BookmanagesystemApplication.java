91672039c6b09066e66809b9b3bdf071eda81eba
biogss <2855971203@qq.com>
2020-04-16 23:17:04 +0800

    Initial commit

---
+++ src/main/java/com/example/bookmanagesystem/BookmanagesystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmanagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmanagesystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmanagesystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookmanagesystem/BookmanagesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmanagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagesystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

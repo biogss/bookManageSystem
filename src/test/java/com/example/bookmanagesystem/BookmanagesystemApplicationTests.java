b02c084491f0ad9f97a2f916b18db2fbef75a9a7
biogss <2855971203@qq.com>
2020-05-10 23:42:27 +0800

    编写redis测试类，学习string，hash的命令

--- src/test/java/com/example/bookmanagesystem/BookmanagesystemApplicationTests.java
+++ src/test/java/com/example/bookmanagesystem/BookmanagesystemApplicationTests.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import redis.clients.jedis.Jedis;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 @SpringBootTest
 class BookmanagesystemApplicationTests {
 
@@ -18,12 +22,63 @@ class BookmanagesystemApplicationTests {
 
     @Test
     void contextLoads() {
-        for (int i = 0; i < 100; i++) {
-            Jedis jedis = redisUtil.getJedis();
-            logger.error(jedis.set("app",jedis.toString()));
-            logger.error(jedis.get("app"));
-            jedis.close();
-        }
+//        不释放链接会因为链接不够报错
+//        for (int i = 0; i < 100; i++) {
+//            Jedis jedis = redisUtil.getJedis();
+//            logger.error(jedis.set("app",jedis.toString()));
+//            logger.error(jedis.get("app"));
+//            jedis.close();
+//        }
+
+//        STRING
+//        Jedis jedis = redisUtil.getJedis();
+//        logger.info(jedis.set("hello","redis"));
+//        logger.info(String.valueOf(jedis.exists("hello")));
+//        logger.info(jedis.get("hello"));
+//        logger.info(jedis.get("hello1"));
+//        logger.info(jedis.setex("java", 100, "study"));
+//        logger.info(String.valueOf(jedis.setnx("java", "studyhard")));
+//        logger.info(String.valueOf(jedis.setnx("python", "studyhard")));
+//        logger.info(jedis.mset("a", "1", "b", "2", "c", "jedis"));
+//        List<String> result = jedis.mget("a","b","c");
+//        result.stream().forEach((str) -> {
+//            logger.info(str);
+//        });
+//        logger.info(String.valueOf(jedis.ttl("a")));
+//        jedis.set("num", 1);//ERROR
+//        logger.info(jedis.type("NUM"));
+//        logger.info(jedis.type("num"));
+//        jedis.set("exp", "timeout", "nx", "ex", 1000);
+//        logger.info(String.valueOf(jedis.strlen("exp")));
+//        HASH
+        Jedis jedis = redisUtil.getJedis();
+//        logger.info(String.valueOf(jedis.hset("personHash","name", "wanghao")));
+//        jedis.hset("personHash", "age", "12");
+//        List<String> hmget = jedis.hmget("personHash", "name", "age", "sex");
+//        hmget.stream().forEach((str) -> {
+//            logger.info(str);
+//        });
+//        Map<String, String> personHash = jedis.hgetAll("personHash");
+//        personHash.forEach( (key, val) -> {
+//            logger.info(key);
+//            logger.info(val);
+//        });
+//        jedis.hset("personHash", "ageH", "11");
+//        Set<String> age = jedis.hkeys("personHash");
+//        age.stream().forEach((str) -> {
+//            logger.info(str);
+//        });
+//        Set<Map.Entry<String, String>> entrySet = personHash.entrySet();
+//        entrySet.stream().forEach((entry) -> {
+//            logger.info(entry.getKey());
+//            logger.info(entry.getValue());
+//        });
+
+
+
+
     }
+
+
 
 }
